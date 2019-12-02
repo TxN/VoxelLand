803ed3e3c27ea96b305a040353397c94de2c0e45
TxN <anton.grebennikov@matryoshka.com>
2019-12-03 02:31:43 +0700

    Отдельная сцена для запуска сервера в headless режиме
    надо убрать логику запуска headless режима из сцены меню, ей там не место.

---
+++ Assets/Scripts/Utils/HeadlessServerRunner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Voxels.Networking.Utils;
+
+namespace Voxels.Utils {
+	public sealed class HeadlessServerRunner : MonoBehaviour {
+		void Start() {
+			if ( !Application.isBatchMode ) {
+				SceneManager.LoadScene("MainMenu");
+				return;
+			}
+			Application.targetFrameRate = 120;
+			NetworkOptions.ServerIP = "127.0.0.1";
+			NetworkOptions.StartServer = true;
+			SceneManager.LoadScene("NetworkGame");
+		}
+	}
+}
