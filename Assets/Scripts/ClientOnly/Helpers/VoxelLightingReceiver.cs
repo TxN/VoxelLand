a19a7d1e56b705f2561ef9ffd0ccddc894393546
anton.grebennikov <anton.grebennikov@matryoshka.com>
2023-02-26 21:20:08 +0700

    Сущности теперь тоже лучше освещаются

--- Assets/Scripts/ClientOnly/Helpers/VoxelLightingReceiver.cs
+++ Assets/Scripts/ClientOnly/Helpers/VoxelLightingReceiver.cs
@@ -10,6 +10,19 @@ namespace Voxels {
 			var cwc = ClientWorldStateController.Instance;
 			return (byte) Mathf.Max(block.LightLevel, block.SunLevel * cwc.AmbientLightIntensity );
 		}
+
+		public byte GetAmbientLightLevel() {
+			var cm = ClientChunkManager.Instance;
+			var block = cm.GetBlockIn(transform.position);
+			var cwc = ClientWorldStateController.Instance;
+			return (byte)(block.SunLevel * cwc.AmbientLightIntensity);
+		}
+
+		public byte GetBlockLightLevel() {
+			var cm = ClientChunkManager.Instance;
+			var block = cm.GetBlockIn(transform.position);
+			return (byte)block.LightLevel;
+		}
 	}
 }
 
--- Assets/Scripts/ClientOnly/Helpers/VoxelLitEntity.cs
+++ Assets/Scripts/ClientOnly/Helpers/VoxelLitEntity.cs
@@ -7,20 +7,24 @@ namespace Voxels {
 		public Material       BaseMaterial = null;
 		public List<Renderer> ModelParts   = new List<Renderer>();
 
-		Material _material = null;
-		float _lerpLevel = 0f;
+		Material _material    = null;
+		float _lerpAmbLevel   = 0f;
+		float _lerpBlockLevel = 0f;
 
 		void Start() {
 			_material = Instantiate(BaseMaterial);
 			foreach ( var part in ModelParts ) {
 				part.material = _material;
 			}
-			_lerpLevel = GetLightLevel() / (float)255;
+			_lerpAmbLevel = GetAmbientLightLevel() / (float)255;
+			_lerpBlockLevel = GetBlockLightLevel() / (float)255;
 		}
 
 		void Update() {
-			_lerpLevel = Mathf.Lerp(_lerpLevel, GetLightLevel() / (float)255, 3f * Time.deltaTime);
-			_material.SetFloat("_LightLevel", _lerpLevel);
+			_lerpAmbLevel = Mathf.Lerp(_lerpAmbLevel, GetAmbientLightLevel() / (float)255, 3f * Time.deltaTime);
+			_lerpBlockLevel = Mathf.Lerp(_lerpBlockLevel, GetBlockLightLevel() / (float)255, 3f * Time.deltaTime);
+			_material.SetFloat("_LightLevel", _lerpBlockLevel);
+			_material.SetFloat("_Daylight", _lerpAmbLevel);
 		}
 	}
 }
